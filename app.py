6887914b1f44df83dc47c52259a6f024c3d2f334
ekaterina lamzina <katelamzina79@gmail.com>
2026-03-03 15:01:54 +0300

    feature-a: add multiply function

---
+++ app.py
@@ -0,0 +1,8 @@
+def greet(name: str) -> str:
+	return f"Hello, {name}!"
+if __name__ == "__main__":
+	print (greet("World"))
+def farewell(name: str) -> str:
+	return f"Goodbye, {name}!"
+def multiplay(a: float, b: float) -> float:
+	return a*b
